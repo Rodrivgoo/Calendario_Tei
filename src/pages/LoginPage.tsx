29976ceb262e1422b924061b6e2da75de278137c
Rodrigo <rodrigo.gomezr21@gmail.com>
2023-11-22 21:58:44 -0300

    Update Login

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,9 +1,13 @@
 // LoginPage.tsx
 import React from 'react';
+import { LogoCalendar } from '@/Images';
 
 const LoginPage: React.FC = () => {
   return (
-    <div className="login-page">
+    <div style={{ textAlign: 'center' }}>
+      <LogoCalendar />
+      <div className="login-page">
+      </div>
     </div>
   );
 };
